791c103828f1846d8774a94077bc4a2385243167
HoangHoi <hoanghoi1310@gmail.com>
2016-02-19 00:38:18 +0700

    Thêm phần sau vào project. Chẳng hiểu sao thư mục vendor không thêm được.

---
+++ public/js/play-sound.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function playSound(btn) {
+    var src_mp3 = btn.attr("data-src-mp3");
+//    var src_ogg = btn.attr("data-src-ogg");
+
+    if (supportAudioHtml5()){
+        playHtml5(src_mp3);
+    }else{
+        alert("Browser don't support HTML5!");
+    }
+}
+
+function supportAudioHtml5(){
+    var audioTag  = document.createElement('audio');
+    try{
+        return ( !!(audioTag.canPlayType)
+                 &&( audioTag.canPlayType("audio/mpeg") != "no" && audioTag.canPlayType("audio/mpeg") != "" ) );     
+    }catch(e){
+        return false;
+    } 
+}
+
+function playHtml5(src_mp3){
+    //use appropriate source
+    var audio = new Audio("");
+    if (audio.canPlayType("audio/mpeg") != "no" && audio.canPlayType("audio/mpeg") != "")
+        audio = new Audio(src_mp3);
+//    else if (audio.canPlayType("audio/ogg") != "no" && audio.canPlayType("audio/ogg") != "")
+//        audio = new Audio(src_ogg);
+
+    //play
+    audio.addEventListener("error", function(e){alert("Apologies, the sound is not available.");});
+    audio.play();
+//    audio.play();
+}
